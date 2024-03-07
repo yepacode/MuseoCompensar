cf28be760babdba39d370033cd7c48d777123433
yepacode <108475443+yepacode@users.noreply.github.com>
2024-03-06 22:11:10 -0500

    creación front museo compensar
    creación parte grafica visitantes museo nacional

---
+++ MuseoCompensar/src/main/java/sumaparesgui/museocompensar/MuseoCompensar.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sumaparesgui.museocompensar;
+
+/**
+ *
+ * @author yepav
+ */
+public class MuseoCompensar {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
